6e9d79588a5bb8e7e2d4d23cdb0abcfff3fc0a69
pitdeer <sarna0925@gmail.com>
2018-02-24 15:31:28 +0100

    Removing soft link

--- provision.sh
+++ provision.sh
@@ -2,7 +2,8 @@ apt-get -y update
 
 apt-get -y install nginx
 
-rm -rf /usr/share/nginx/www/
-ln -s /vagrant/www /usr/share/nginx
+rm -rf /etc/nginx/sites-enabled
+cp -r /vagrant/sites-enabled /etc/nginx
+
 
 service nginx start
